74563dc6bbab70d5f44858db1b998254d32243c7
nitinsayshe <nitinsayshe@gmail.com>
2022-06-07 11:54:23 +0530

    my final push

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,33 +1,40 @@
 const BookModel= require("../models/bookModel")
 
+
+//Create New Entry of Book 
 const createBook= async function (req, res) {
     let data= req.body
     let savedData= await BookModel.create(data)
     res.send({msg: savedData})
 }
-
+// Fetch The data from DB, (bookName And authorName)
 const getBooksData= async function (req, res) {
-    let allBooks= await BookModel.find().select("bookName authorName")
+    let allBooks= await BookModel.find().select("bookName authorName ")
     res.send({msg: allBooks})
 }
 
+// Fetch 
 const getBooksInYear=async function(req,res){
     //let allBooks =await BookModel.find({year:req.query.year})
     let allBooks =await BookModel.find({year:req.body.year})
     res.send({msg: allBooks})
      }
 
-const getParticularBooks=  async function(req,res){
+const getParticularBooks=  async function(req,res){ 
         let name=req.body.bookName;
         let year=req.body.year;
-        let allBooks =await BookModel.find({$or:[{bookName:name},{year:year}]})
+        //let allBooks =await BookModel.find({$or:[{bookName:name},{year:year}]})
+        let allBooks=await BookModel.find(req.body)
         console.log(name,year)
         res.send({msg:allBooks})}
 
 const getXINRBooks =async function(req,res){
-    let allBooks =await BookModel.find({$or:[{"prices.indianPrice":{$eq:"100INR"}},
-    {"prices.indianPrice":{$eq:"200INR"}},
-    {"prices.indianPrice":{$eq:"500INR"}}]})
+
+    // let allBooks =await BookModel.find({$or:[{"prices.indianPrice":{$eq:"100INR"}},
+    // {"prices.indianPrice":{$eq:"200INR"}},
+    // {"prices.indianPrice":{$eq:"500INR"}}]})
+
+    let allBooks =await BookModel.find({"prices.indianPrice":{$in:["100INR","200INR","500INR"]}})
     res.send({msg:allBooks})
 }
 
--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-const bookSchema = new mongoose.Schema( {
+
+//-------------Book Model Schema--------------//
+const bookSchema = new mongoose.Schema( {  
     bookName: {
         type: String,
         required:true
